64a4bbc6420a27066480a4878ef3465d1b8fb8cb
yogeshwakle95 <yogeshwakle95@gmail.com>
2024-07-08 21:47:06 +0530

    leap year code

---
+++ Basics_Flow/Leap_year.java
@@ -0,0 +1,30 @@
+package JAVA_DSA.Basics_Flow;
+//  Input a year and find whether it is a leap year or not.
+public class Leap_year {
+    public static void main(String[] args) {
+
+        // year to be checked;
+        int year = 2012;
+        boolean leap = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    leap = true;
+                } else {
+                    leap = false;
+                }
+                leap = true;
+            } else {
+                leap = false;
+            }
+            leap = true;
+        } else {
+            leap = false;
+        }
+        if(leap){
+            System.out.println(year +" year is leap");
+        }else{
+            System.out.println(year+ " year not a leap");
+        }
+    }
+}
